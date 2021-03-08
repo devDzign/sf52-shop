e66c5ab625470eb8920aa1e5bc5069b30b3fa0ff
devDzign <chabour.mourad87@gmail.com>
2021-03-08 03:52:10 +0100

    feat(template):
     - intégration de template

--- assets/app.js
+++ assets/app.js
@@ -9,5 +9,23 @@ import './styles/app.scss';
 // srr
 import './ssr-all'
 
+// theme js
+import './js/themes-js/scripts'
+import './js/themes-js/popper.min'
+import './owlcarousel/js/owl.carousel.min'
+import './js/themes-js/magnific-popup.min'
+import './js/themes-js/waypoints.min'
+import './js/themes-js/parallax'
+import './js/themes-js/jquery.countdown.min'
+import './js/themes-js/imagesloaded.pkgd.min'
+import './js/themes-js/isotope.min'
+import './js/themes-js/jquery.dd.min'
+import './js/themes-js/slick.min'
+import './js/themes-js/jquery.elevatezoom'
+
+// import './js/themes-js/isotope-loadmore'
+// import './js/themes-js/jquery-ui'
+// import './js/themes-js/jquery.fitvids'
+
 // start the Stimulus application
 import './bootstrap';
